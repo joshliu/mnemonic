431e7bd75765b6df9e41df828eaf734696910065
Joshua Liu <joshliu6830@gmail.com>
2017-04-01 16:13:27 -0400

    Initial Commit

---
+++ Mnemonic/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Mnemonic
+//
+//  Created by Joshua Liu on 4/1/17.
+//  Copyright © 2017 Joshua Liu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
